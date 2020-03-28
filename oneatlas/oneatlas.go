1627f972b71068644df4d1010a0c68ac00309c1c
Yakov Zaytsev <me@ysz.io>
2020-03-28 18:00:41 +0300

    add ExampleSearch

--- oneatlas/oneatlas.go
+++ oneatlas/oneatlas.go
@@ -1,5 +1,69 @@
 package oneatlas
 
-func Hello() string {
-	return "Hello, world"
+import "encoding/json"
+import "errors"
+import "log"
+import "net/http"
+import "net/url"
+
+type Link struct {
+	Href string `json:"href"`
+}
+
+type links struct {
+	Delete          Link `json:"delete"`
+	ImagesGetBuffer Link `json:"imagesGetBuffer"`
+}
+
+type Feature struct {
+	links `json:"_links"`
+}
+
+type featureCollection struct {
+	features []Feature
+}
+
+type Client struct {
+	// Base URL for API requests
+	BaseURL *url.URL
+
+	httpClient *http.Client
+}
+
+type oneatlasError struct {
+	Message string `json:"message"`
+}
+
+func (c *Client) Search() ([]Feature, error) {
+	u := c.BaseURL
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, nil
+	}
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		var err oneatlasError
+		json.NewDecoder(resp.Body).Decode(&err)
+		return nil, errors.New(err.Message)
+	}
+
+	var fC featureCollection
+	err = json.NewDecoder(resp.Body).Decode(&fC)
+	return fC.features, err
+}
+
+func NewClient(httpClient *http.Client) *Client {
+	u, err := url.Parse("https://search.oneatlas.geoapi-airbusds.com/api/v1/opensearch")
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := &Client{httpClient: httpClient, BaseURL: u}
+	return c
 }
--- oneatlas/oneatlas_test.go
+++ oneatlas/oneatlas_test.go
@@ -1,10 +1,34 @@
 package oneatlas
 
-import "testing"
+import "fmt"
+import "golang.org/x/oauth2"
+import "os"
 
+//import "testing"
+
+/*
 func TestHelloWorld(t *testing.T) {
 	want := "Hello, world."
 	if got := Hello(); got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
+}
+*/
+
+func ExampleSearch() {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("ACCESS_TOKEN")},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client := NewClient(tc)
+
+	features, err := client.Search()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", features)
+
+	// Output:
+	// TODO
 }
